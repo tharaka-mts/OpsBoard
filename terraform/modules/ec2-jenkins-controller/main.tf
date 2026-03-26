cf92c1886164166b5f31b1cb931ddf1f5202990c
Tharaka Sandaruwan <tharaka.mts@gmail.com>
2026-03-26 21:29:40 +0530

    feat: terraform jenkins module

---
+++ terraform/modules/ec2-jenkins-controller/main.tf
@@ -0,0 +1,34 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "this" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+
+  vpc_security_group_ids = [var.security_group_id]
+  key_name               = var.key_pair_name
+
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "${var.project_name}-jenkins-controller"
+  }
+}
