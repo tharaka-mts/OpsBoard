f3f9253b9f2a3bc0eb774b9142cca869e1311d9e
Tharaka Sandaruwan <tharaka.mts@gmail.com>
2026-03-30 20:35:49 +0530

    fix: destroy un empty ecr

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "backend" {
   name                 = "${var.project_name}-backend"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -10,6 +11,7 @@ resource "aws_ecr_repository" "backend" {
 resource "aws_ecr_repository" "frontend" {
   name                 = "${var.project_name}-frontend"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
